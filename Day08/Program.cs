96e828db499e4633be060d1e7a547707539930eb
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2020-12-08 09:27:32 +0100

    AoC Day08 2020 in C#

---
+++ Day08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("Input/part1.txt")
+                .Split(new string[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => t.Trim()).ToList();
+            var today = new Day08(data);
+            
+            Console.Write("Part 1: ");
+            Console.WriteLine(today.ComputePart1());
+            Console.Write("Part 2: ");
+            Console.WriteLine(today.ComputePart2());
+        }
+    }
+}
